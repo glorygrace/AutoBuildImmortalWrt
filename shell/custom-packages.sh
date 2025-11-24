eb12b36cdc75088c428ac0b00144e65fafdeaf33
glorygrace <1025620143@qq.com>
2025-11-24 17:01:28 +0800

    Update custom-packages.sh
    # 首页和网络向导
    CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -9,7 +9,7 @@
 #去除晶晨宝盒
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-app-amlogic
 # 首页和网络向导
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
 # 高级卸载 by YT Vedio Talk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-uninstall"
 # 极光主题 by github eamonxg
